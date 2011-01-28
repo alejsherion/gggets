230015b999ded82cfd2ebeacef105d8082d1322e
william.wu1982 <william.wu1982@b8608e59-ae43-5b33-6f67-ec1891135042>
2011-01-28 09:05:01 +0000

    添加遗漏代码文件

---
+++ trunk/GGGETSApp/Application.GGETS/CRM/ICompanyUserManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ETS.GGGETSApp.Domain.Application.Entities;
+
+namespace Application.GGETS
+{
+    public interface ICompanyUserManagementService
+    {
+        void CreateNewCompanyUser(CompanyUser companyUser);
+        void AddCompanyUserToExistingCompany(CompanyUser companyUser);
+        void ChangeCompanyUser(CompanyUser companyUser);
+        void RemoveCompanyUser(CompanyUser companyUser);
+    }
+}
---
+++ trunk/GGGETSApp/Domain.GGGETS/CRM/ICompanyUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ETS.GGGETSApp.Domain.Core;
+using ETS.GGGETSApp.Domain.Application.Entities;
+
+
+namespace Domain.GGGETS
+{
+    public interface ICompanyUserRepository:IRepository<CompanyUser>
+    {
+        CompanyUser FindCompanyUserByLoginName(string loginName);
+        
+    }
+}
