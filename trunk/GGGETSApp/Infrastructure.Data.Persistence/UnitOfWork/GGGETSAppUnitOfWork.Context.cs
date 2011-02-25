aff3860b85f27ebd59a90a27c1c28fc91c1fa373
positionwanted@126.com <positionwanted@126.com@b8608e59-ae43-5b33-6f67-ec1891135042>
2011-02-25 09:55:16 +0000

    更新数据建模！

--- trunk/GGGETSApp/Infrastructure.Data.Persistence/UnitOfWork/GGGETSAppUnitOfWork.Context.cs
+++ trunk/GGGETSApp/Infrastructure.Data.Persistence/UnitOfWork/GGGETSAppUnitOfWork.Context.cs
@@ -249,6 +249,18 @@ namespace ETS.GGGETSApp.Infrastructure.Data.Persistence.UnitOfWork
             get { return _user  ?? (_user = CreateObjectSet<User>("User")); }
         }
         private ObjectSet<User> _user;
+    
+        public IObjectSet<AppModule> AppModule
+        {
+            get { return _appModule  ?? (_appModule = CreateObjectSet<AppModule>("AppModule")); }
+        }
+        private ObjectSet<AppModule> _appModule;
+    
+        public IObjectSet<Privilege> Privilege
+        {
+            get { return _privilege  ?? (_privilege = CreateObjectSet<Privilege>("Privilege")); }
+        }
+        private ObjectSet<Privilege> _privilege;
 
         #endregion
     }
