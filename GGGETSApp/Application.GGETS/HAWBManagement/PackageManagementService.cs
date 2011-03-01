6564e4cb6400521a873fc64379ef1ec62d743f2f
positionwanted <positionwanted@126.com>
2011-03-01 01:33:55 +0000

    包裹和运单之间的逻辑代码！

--- GGGETSApp/Application.GGETS/HAWBManagement/PackageManagementService.cs
+++ GGGETSApp/Application.GGETS/HAWBManagement/PackageManagementService.cs
@@ -82,5 +82,54 @@ namespace Application.GGETS
         {
             return _packageRepository.FindPackageByCondition(barCode, beginDate, endDate, destinationCode);
         }
+
+        /// <summary>
+        /// 判断加入的运单的PID是否为NULL，如果是NULL才可以加入包裹，否则等于重复添加运单
+        /// </summary>
+        /// <param name="barcode">运单编号</param>
+        /// <returns></returns>
+        public bool JudgePIDIsNull(string barcode)
+        {
+            bool judge = false;
+            HAWB hawb = _hawbRepository.FindHAWBByBarCode(barcode);
+            if(hawb!=null)
+            {
+                if (!string.IsNullOrEmpty(Convert.ToString(hawb.PID))) judge = true;
+            }
+            return judge;
+        }
+
+        /// <summary>
+        /// 判断包裹和运单的三字码是否重复
+        /// </summary>
+        /// <param name="barcode">运单编号</param>
+        /// <param name="packageRegionCode">包裹三字码</param>
+        /// <param name="isMix">是否为混包</param>
+        /// <returns></returns>
+        public bool JudgeRegionCodeIsRepeat(string barcode, string packageRegionCode, bool isMix)
+        {
+            bool judge = false;
+            if (isMix) return true;
+            else
+            {
+                //首先获取运单对象
+                HAWB hawbObj = _hawbRepository.FindHAWBByBarCode(barcode);
+                if (hawbObj != null)
+                {
+                    if(!string.IsNullOrEmpty(hawbObj.DeliverRegion))
+                    {
+                        if (hawbObj.DeliverRegion.Equals(packageRegionCode)) return true;
+                    }
+                    else
+                    {
+                        if(!string.IsNullOrEmpty(hawbObj.ConsigneeRegion))
+                        {
+                            if (hawbObj.ConsigneeRegion.Equals(packageRegionCode)) return true;
+                        }
+                    }
+                }
+            }
+            return judge;
+        }
     }
 }
