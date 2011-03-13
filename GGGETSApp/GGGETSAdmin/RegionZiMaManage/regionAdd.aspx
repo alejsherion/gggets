9283667ee65f9e37f077dd0ec35abc4e12853ea6
wx8849 <wx8849@163.com>
2011-03-13 14:18:55 +0000

    地区三字码管理

---
+++ GGGETSApp/GGGETSAdmin/RegionZiMaManage/regionAdd.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="regionAdd.aspx.cs" Inherits="GGGETSAdmin.RegionZiMaManage.regionAdd" Theme="logisitc" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc2" %>
+
+<%@ Register Assembly="AutoCompleteExtra" Namespace="AutoCompleteExtra" TagPrefix="cc1" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <cc2:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+    </cc2:ToolkitScriptManager>
+     <div class="">
+        <table class="DataView">
+            <tr class="Row">
+                <td class="FieldHeader" style="width:120px">
+                    <asp:Label ID="lbl_CountryCode" runat="server" Text="国家二字码:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="Txt_CountryCode" runat="server" Width="80" TabIndex="1" 
+                        style="text-transform:uppercase" MaxLength="2"></asp:TextBox>
+                        <cc1:AutoCompleteExtraExtender runat="server" ID="autocomplete" ServiceMethod="GetCountryList"
+                                        TargetControlID="Txt_CountryCode" AsyncPostback="false" AutoPostback="true"
+                                        MinimumPrefixLength="1" CompletionSetCount="10" onitemselected="autocomplete_ItemSelected">
+                                    </cc1:AutoCompleteExtraExtender>
+                </td>
+            </tr>
+            <tr class="AlternatingRow">
+                <td class="FieldHeader">
+                    <asp:Label ID="lbl_RegionCode" runat="server" Text="地区三字码:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txt_RegionCode" runat="server"
+                        TabIndex="2" MaxLength="3" style="text-transform:uppercase"></asp:TextBox>
+                </td>
+               
+            </tr>
+            <tr class="Row">
+                <td class="FieldHeader">
+                    <asp:Label ID="lbl_RegionName" runat="server" Text="地区全称:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txt_RegionName" runat="server"
+                        TabIndex="3" style="text-transform:uppercase" MaxLength="50"></asp:TextBox>
+                </td>
+               
+            </tr>
+        </table>
+        <div class="FooterBtnBar">
+            <asp:Button ID="btn_Add" runat="server" CssClass="InputBtn" Text="添 加" 
+                        onclick="btn_Add_Click" TabIndex="4"/>
+        </div>
+    </div>
+</asp:Content>
---
+++ GGGETSApp/GGGETSAdmin/RegionZiMaManage/regionAdd.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace GGGETSAdmin.RegionZiMaManage {
+    
+    
+    public partial class regionAdd {
+        
+        /// <summary>
+        /// ToolkitScriptManager1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::AjaxControlToolkit.ToolkitScriptManager ToolkitScriptManager1;
+        
+        /// <summary>
+        /// lbl_CountryCode 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_CountryCode;
+        
+        /// <summary>
+        /// Txt_CountryCode 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_CountryCode;
+        
+        /// <summary>
+        /// autocomplete 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::AutoCompleteExtra.AutoCompleteExtraExtender autocomplete;
+        
+        /// <summary>
+        /// lbl_RegionCode 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_RegionCode;
+        
+        /// <summary>
+        /// txt_RegionCode 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_RegionCode;
+        
+        /// <summary>
+        /// lbl_RegionName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_RegionName;
+        
+        /// <summary>
+        /// txt_RegionName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_RegionName;
+        
+        /// <summary>
+        /// btn_Add 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Add;
+    }
+}
