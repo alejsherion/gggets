a2edb27baed982cd6e6cd63125d30c55a1c6feb1
positionwanted <positionwanted@126.com>
2011-03-24 07:18:45 +0000

    遗漏

---
+++ GGGETSApp/Domain.Entities/Partial/RolePartial.cs
@@ -0,0 +1,35 @@
+﻿//************************************************************************
+// 用户名				GGGETS国际综合快递
+// 系统名				管理后台
+// 子系统名		        角色
+// 作成者				hong.li
+// 改版日				2011.02.16
+// 改版内容				创建并且修改
+//************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ETS.GGGETSApp.Domain.Application.Entities
+{
+    #region 枚举类型
+    /// <summary>
+    /// 角色状态
+    /// </summary>
+    public enum RoleStatus
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        冻结=0,
+        /// <summary>
+        /// 
+        /// </summary>
+        正常=1
+    }
+    #endregion
+    public partial class RolePartial
+    {
+    }
+}
