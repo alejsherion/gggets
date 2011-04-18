fbe6fbd0344cbb9e9571fea7f8e21afb7d729023
positionwanted <positionwanted@126.com>
2011-04-18 17:02:03 +0000

    报关人员选择航班主界面以及报关文件导入

---
+++ GGGETSApp/GGGETSAdmin/CustomsClearance/ClearanceImport.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ClearanceImport.aspx.cs" Inherits="GGGETSAdmin.CustomsClearance.ClearanceImport" %>
+
+<%@ Register src="../SWFUpload/UC_SWFUpload.ascx" tagname="UC_SWFUpload" tagprefix="uc1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>导入报关文件</title>
+    <script>
+        //判断状态是否存在于cookie中
+        function IsExistCookieStatus() {
+            if (getCookie("uploadStatus") == '200') {
+                //删除cookie
+                alert("报关文件已经上传到服务器，请点击获取数据！");
+                TeleMallAdmin.PickupTicket.ExcelImport.DeleteCookie();
+                document.getElementById("divBtn").style.display = "block";
+                document.getElementById("divSwf").style.display = "none";
+                clearInterval(id);
+            }
+        }
+
+        //得到cookie
+        function getCookie(objName) {//获取指定名称的cookie的值
+            var arrStr = document.cookie.split("; ");
+            for (var i = 0; i < arrStr.length; i++) {
+                var temp = arrStr[i].split("=");
+                if (temp[0] == objName) return unescape(temp[1]);
+            }
+        }
+        var id = setInterval(IsExistCookieStatus, 200);
+    </script>
+    <link href="../Styles/sub.css" rel="stylesheet" type="text/css" />
+    <link href="../SWFUpload/css/swfupload.css" rel="stylesheet" type="text/css" />
+    <script src="../SWFUpload/js/swfupload.js" type="text/javascript"></script>
+    <script src="../SWFUpload/js/handlers.js" type="text/javascript"></script>
+    <script src="../SWFUpload/js/json2.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <div>
+    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" class="table3">
+  <tr>
+    <td class="title">导入报关文件</td>
+  </tr>
+  <tr>
+    <td colspan="4" bgcolor="#FFFFFF" class="STYLE1">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" style="margin-top:10px;">
+      <tr>
+        <td class="lf">Excel文件上传：</td>
+           <td nowrap="nowrap">
+               <div id="divSwf">
+                    <uc1:uc_swfupload ID="UC_SWFUpload1" runat="server" />
+               </div>
+               <div id="divBtn" style="display:none;">
+                    <asp:Button ID="btn" runat="server" Text="获取上传文件数据" onclick="btn_Click" />
+               </div>
+           </td>
+      </tr>
+      <tr>
+        <td class="lf">航班编号：</td>
+           <td nowrap="nowrap">
+              <asp:Label ID="lblFlightNo" runat="server" Text=""></asp:Label></td>
+      </tr>
+      <tr>
+        <td class="lf">运单数量：</td>
+           <td nowrap="nowrap" align="left"><asp:Label ID="lblHAWBNum" runat="server" Text=""></asp:Label></td>
+      </tr>
+      <tr id="trMessage" runat="server">
+        <td nowrap="nowrap" style=" height:30px;" colspan="2">
+            <!--GRID-->
+            <telerik:RadGrid ID="RGGetData" runat="server">
+            </telerik:RadGrid>
+        </td>
+      </tr>
+      <tr>
+        <td nowrap="nowrap" style=" width:20%;height:30px;">
+          </td>
+           <td height="50" nowrap="nowrap">
+                <asp:Button ID="btnConfirm" runat="server" Text="返回" class="butt" onclick="btnConfirm_Click" Visible="false"/>&nbsp;
+                <div id="result" runat="server" style=" color:Red;" />
+          </td>
+      </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GGGETSApp/GGGETSAdmin/SWFUpload/upload.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GGGETSAdmin.Common;
+using IMMENSITY.SWFUploadAPI;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+public partial class SWFUpload_upload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string cmd = SWFUrlOper.GetStringParamValue("cmd");
+            if (cmd == string.Empty)
+                this.uploadFile();
+            else if (cmd == "del")
+                this.DelFile();
+        }
+    }
+
+    public void uploadFile()
+    {
+        //保存的路径
+        string savePath = SWFUrlOper.GetFormStringParamValue("path");
+        //旧文件名,方便删除
+        string oldFileName = SWFUrlOper.GetFormStringParamValue("fn");
+        //是否需要小图
+        bool isSmall = SWFUrlOper.GetFormStringParamValue("small").ToLower() == "true" ? true : false;
+        //是否需要水印
+        bool isWaterMark = SWFUrlOper.GetFormStringParamValue("wm").ToLower() == "true" ? true : false;
+        //小图宽度
+        int smallWidth = SWFUrlOper.GetFormIntParamValue("sw");
+        //小图高度
+        int smallHeight = SWFUrlOper.GetFormIntParamValue("sh");
+        //图片扩展名
+        string[] imgExtension = new string[] { "jpg", "gif", "png", "bmp" };
+        //已上传文件的文件名
+        string nameList = SWFUrlOper.GetFormStringParamValue("data");
+        try
+        {
+            // 获取上传的文件信息
+            HttpPostedFile file_upload = Request.Files["Filedata"];
+            string extension = string.Empty;
+            string fileName = string.Empty;
+            //bool isImg = false;
+            if (file_upload.ContentLength > 0)
+            {
+                fileName = file_upload.FileName;
+                if (fileName.IndexOf(".") != -1)
+                    extension = fileName.Substring(fileName.LastIndexOf(".") + 1, fileName.Length - fileName.LastIndexOf(".") - 1).ToLower();
+
+                SWFUploadFile uf = new SWFUploadFile();
+                if (isSmall)
+                {
+                    uf.SmallPic = true;
+                    uf.MaxWith = smallWidth == 0 ? uf.MaxWith : smallWidth;
+                    uf.MaxHeight = smallHeight == 0 ? uf.MaxHeight : smallHeight;
+                }
+                uf.IsWaterMark = isWaterMark;
+                int state = 0;
+                string newFileName = uf.SaveFile(file_upload, savePath, oldFileName, ref state);
+
+                //0:上传成功.  1:没有选择要上传的文件.  2:上传文件类型不符.   3:上传文件过大  -1:应用程序错误.
+                int statusCode = 500;
+                switch (state)
+                {
+                    case 0:
+                        statusCode = 200;
+                        break;
+                    case 1:
+                        statusCode = 200;
+                        break;
+                    default:
+                        statusCode = 500;
+                        break;
+                }
+
+                List<SWFUploadFileInfo> listufi = new List<SWFUploadFileInfo>();
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<SWFUploadFileInfo>));
+                if (nameList != string.Empty)
+                    using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(nameList)))
+                        listufi = (List<SWFUploadFileInfo>)serializer.ReadObject(ms);//反序列化
+
+                #region 得到图片尺寸
+                int imgWidth = 0;
+                int imgHeight = 0;
+                foreach (string item in imgExtension)
+                {
+                    if (item == extension && file_upload.InputStream != null)
+                    {
+                        System.Drawing.Image img = System.Drawing.Image.FromStream(file_upload.InputStream);
+                        imgWidth = img.Width;
+                        imgHeight = img.Height;
+                        img.Dispose();
+                    }
+                }
+                #endregion
+
+                listufi.Add(new SWFUploadFileInfo()
+                {
+                    Id = listufi.Count,
+                    FileName = newFileName,
+                    OriginalFileName = fileName,
+                    Path = savePath,
+                    Width = imgWidth,
+                    Height = imgHeight
+                });
+                string postData = "";
+                //序列化
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.WriteObject(stream, listufi);
+                    postData = Encoding.UTF8.GetString(stream.ToArray());
+                }
+
+                Response.StatusCode = statusCode;
+                //状态存入cookie
+                if (statusCode==200) CookiesHelp.Add("uploadStatus", statusCode.ToString());
+                Response.Write(postData);
+            }
+        }
+        catch (Exception e)
+        {
+            //内部服务器错误
+            Response.StatusCode = 500;
+            Response.Write("内部服务器错误:" + e.Message.ToString());
+        }
+        HttpContext.Current.ApplicationInstance.CompleteRequest();
+    }
+    /// <summary>
+    /// 删除文件
+    /// </summary>
+    /// <returns></returns>
+    public void DelFile()
+    {
+        string name = SWFUrlOper.GetStringParamValue("name");
+        string msg = new SWFUploadFile().Delete(SWFUrlOper.GetStringParamValue("f"), SWFUrlOper.GetStringParamValue("name"), true);
+        Response.Write(msg);
+        Response.End();
+    }
+}
