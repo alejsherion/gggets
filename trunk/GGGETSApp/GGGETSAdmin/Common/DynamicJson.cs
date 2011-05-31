5e013b402eb86949864c5a442c17b2c76b9496cd
positionwanted@126.com <positionwanted@126.com@b8608e59-ae43-5b33-6f67-ec1891135042>
2011-05-31 14:08:11 +0000

    JSON转化

---
+++ trunk/GGGETSApp/GGGETSAdmin/Common/DynamicJson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using Newtonsoft.Json.Linq;
+
+namespace GGGETSAdmin.Common
+{
+    public class DynamicJson : DynamicObject
+    {
+        Newtonsoft.Json.Linq.JObject _json;
+        public DynamicJson(Newtonsoft.Json.Linq.JObject json)
+        {
+            _json = json;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            bool ret = false;
+            JToken value;
+            if (_json.TryGetValue(binder.Name, out value))
+            {
+                result = (value as JValue).Value;
+                ret = true;
+            }
+            else
+            {
+                result = null;
+            }
+            return ret;
+        }
+    }
+}
